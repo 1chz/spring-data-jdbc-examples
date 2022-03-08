1fe12820eddc3592ddb7d78afe21932497f7b97c
shirohoo <shirohoo4102@gmail.com>
2022-03-08 23:04:43 +0900

    :fire: CrudRepository -> PagingAndSortingRepository 서브 타이핑

--- src/main/java/io/shirohoo/github/spring/repository/PostRepository.java
+++ src/main/java/io/shirohoo/github/spring/repository/PostRepository.java
@@ -1,7 +1,9 @@
 package io.shirohoo.github.spring.repository;
 
 import io.shirohoo.github.spring.model.Post;
+import org.springframework.data.jdbc.repository.support.SimpleJdbcRepository;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface PostRepository extends CrudRepository<Post, Long> {
+public interface PostRepository extends PagingAndSortingRepository<Post, Long> {
 }
